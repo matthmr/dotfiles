6dd0d5356253eb0c25fe5230bc2873fd54e995cb
mh <observermatth@gmail.com>
2022-07-22 09:38:54 -0300

    [move] move `scripts` to a new repository

--- scripts/find/wiki-find-pacman.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-wiki_find_pacman() {
-	grep -Frwin -f "$1" /home/mh/Documents/Wiki/ --exclude='*.html' --exclude='*.pdf' --exclude='*.png' --exclude-dir=.git --color
-}
-
-wiki_find_pacman "$1"
